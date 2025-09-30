4b9548b5ebcbbee80260db77488127d941026f20
anotherwinter <kolobok.sloupok@gmail.com>
2025-09-30 03:02:33 +0300

    test changes

--- compare.cjs
+++ compare.cjs
@@ -1,6 +1,7 @@
 function compare(a, b)
 {
-  const length = Math.max(a.length, b.length);
+  const length = Math.min(a.length, b.length);
+  
   for (let i = 0; i < length; ++i) {
     const ca = i < a.length ? a.charCodeAt(i) : 0;
     const cb = i < b.length ? b.charCodeAt(i) : 0;
