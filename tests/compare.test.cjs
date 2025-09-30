0aab1429069437f1999d0967c9c6e1c32d4ff38b
anotherwinter <kolobok.sloupok@gmail.com>
2025-09-30 03:30:37 +0300

    add eslint, apply linting

--- tests/compare.test.cjs
+++ tests/compare.test.cjs
@@ -1,7 +1,7 @@
 const compare = require('../compare');
 
 test('compare hello with hello, result 0',
-     () => { expect(compare("hello", "hello")).toBe(0); });
+  () => { expect(compare('hello', 'hello')).toBe(0); });
 
 test('compare can with ban, result >0',
-     () => { expect(compare("can", "ban")).toBe(1); });
+  () => { expect(compare('can', 'ban')).toBe(1); });
