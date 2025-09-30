4029299f33fe75d07f75e3001698a8b160d2c5fa
anotherwinter <kolobok.sloupok@gmail.com>
2025-09-30 05:57:25 +0300

    change react app greeting message

--- test-app/src/App.js
+++ test-app/src/App.js
@@ -1,22 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+function App()
+{
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          This was built & deployed using GH Actions CI/CD pipeline!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
